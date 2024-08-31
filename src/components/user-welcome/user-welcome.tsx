41a756066531fd15f8260e4fb9199351c26c3e5f
Thomas Gransden <thomasgransden@gmail.com>
2024-08-31 19:27:59 +0100

    Remove solution

--- src/components/user-welcome/user-welcome.tsx
+++ src/components/user-welcome/user-welcome.tsx
@@ -3,17 +3,8 @@ type UserWelcomeProps = {
   lastName?: string;
 };
 
-const UserWelcome = ({ firstName, lastName }: UserWelcomeProps) => {
-  const prefix = "Hello";
-
-  const getString = () => {
-    if (!firstName && !lastName) return `${prefix}!`;
-    if (firstName && !lastName) return `${prefix} ${firstName}`;
-    if (firstName && lastName) return `${prefix} ${firstName} ${lastName}`;
-    if (!firstName && lastName) return `Hello!`;
-  };
-
-  return <p>{getString()}</p>;
+const UserWelcome = ({}: UserWelcomeProps) => {
+  return null;
 };
 
 export default UserWelcome;
